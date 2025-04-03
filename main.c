249bcfa1a368c1eb6c6312bde85fc96e00426ec0
MDulche <dulchemaxence422010@gmail.com>
2025-04-03 10:49:27 +0200

    Maxence

--- main.c
+++ main.c
@@ -1,29 +1,45 @@
+//Maxence Dulche - Sarah
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_LINE_LENGTH 6  // Définir une taille de buffer pour fgets
-int IsolatioNumber(char *chaine){
-    int number = 0, index = 0, check = 0;
+#include <stdbool.h>
+#define LENGTH 6  // Définir une taille de buffer pour fgets
+void isolatioNumber (char *chaine, int *tableau){
+
+    int number = 0, index = 0, check = 0, i = 0;
+
     while (check && chaine[index] != '\0'){
+
         if (chaine[index] < '0' || chaine[index] > '9'){
-            check = 1;
-        }
-        if (check){
             number = number * 10 + (chaine[index] - '0');
+        } else if (chaine[index] == ',') {
+            tableau[i] = number;
+            number = 0;
+            i++;
         }
-        index++;
     }
-    return number;
 }
+
 int main() {
+
     char *filename = "mc.txt";
-    int number = 0, index =0 ,tableau = [8], i = 0;
+    int number = 0, index =0 ,tableau[8];
+
     FILE *inputFile = fopen(filename, "r");
-    char buffer[MAX_LINE_LENGTH];  // Buffer pour stocker une ligne
-    while (fgets(buffer, MAX_LINE_LENGTH, inputFile) != NULL) {
-        tableau[i] = IsolatioNumber(buffer);
-        printf("%d\n", tableau[i]);  // Affichage de la ligne
-        i++;
+
+    char chaine[LENGTH];  // Buffer pour stocker une ligne
+
+    fgets(chaine, LENGTH, inputFile); // Recuperation de la ligne de chiffre
+
+    IsolatioNumber(chaine, tableau);
+
+    for (int i = 0; i < LENGTH; i++)
+    {
+        printf("%d\n", tableau[i]);
     }
+
+    
+
+    
     fclose(inputFile);
     return 0;
 }
